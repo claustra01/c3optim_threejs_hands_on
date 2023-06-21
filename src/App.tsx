ff98cad6ad9352ff7d9646426e2d7ea1a9dbc185
claustra01 <shinya430.g@gmail.com>
2023-06-21 18:37:02 +0900

    :sparkles: change viewing models

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,39 @@ import { Canvas } from "@react-three/fiber";
 
 // コンポーネントのインポート
 import Footer from "./Footer";
+import ModelView from "./ModelView";
 
 // caaのインポート
 import "./App.css";
-import ModelView from "./ModelView";
 
 function App() {
+  // 表示する3Dモデルのインデックスの状態
+  const [selectedModel, setSelectedModel] = useState<number>(0);
+  // 3Dモデルのパスを保持する変数
+  const models: string[] = ["/model1.obj", "/model2.obj", "/model3.obj"];
+
   return (
     <div className="App">
+      {/* 3Dモデル切り替え用ボタン */}
+      <div className="model_switch_button">
+        {/* 繰り返し処理 */}
+        {models.map((_, index) => {
+          return (
+            <div
+              className={`${index === selectedModel ? "selected" : ""}`}
+              key={index}
+              // クリックイベントの処理
+              onClick={() => {
+                setSelectedModel(index);
+              }}
+            >
+              {index + 1}
+            </div>
+          );
+        })}
+      </div>
       <Canvas className="canvas">
-        <ModelView model_url="/model1.obj" key={0} />
+        <ModelView model_url={models[selectedModel]} key={selectedModel} />
       </Canvas>
       <Footer />
     </div>
--- src/ModelView.tsx
+++ src/ModelView.tsx
@@ -4,13 +4,29 @@ import { useLoader } from "@react-three/fiber";
 import { Mesh } from "three";
 import { OrbitControls as OrbitControlImpl } from "three-stdlib";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
-import { OrbitControls, Preload, PerspectiveCamera } from "@react-three/drei";
+import {
+  Backdrop,
+  Center,
+  ContactShadows,
+  OrbitControls,
+  Preload,
+  PerspectiveCamera,
+  useProgress,
+  Html,
+} from "@react-three/drei";
 
 // ModelViewのpropsの型定義
 type ModelViewProps = {
   model_url: string;
   key: number;
 };
+
+// ローディング要素
+function Loading() {
+  const { progress } = useProgress();
+  console.log(progress + "% loading...");
+  return <Html className="loading"></Html>;
+}
 
 // モデルローダー
 const Model = (args: any) => {
@@ -30,15 +46,25 @@ const Model = (args: any) => {
 function ModelView(props: ModelViewProps) {
   // コントローラー用Refの定義
   const controlRef = useRef<OrbitControlImpl>({} as OrbitControlImpl);
-  //   メッシュ定義
+  // メッシュ定義
   const mesh = useRef({} as Mesh);
 
   return (
     <>
       {/* ローディングが終わるまでは何も表示しない */}
-      <Suspense fallback={null}>
-        {/* 上記で定義したモデルローダーで表示される3Dモデル */}
-        <Model mesh={mesh} model_url={props.model_url} position={[0, 0, 0]} />
+      <Suspense fallback={<Loading />}>
+        <Backdrop
+          receiveShadow
+          scale={[20, 5, 5]}
+          floor={1}
+          position={[0, -0.5, -2]}
+        >
+          <meshPhysicalMaterial roughness={1} color="#a0d8ef" />
+        </Backdrop>
+        <Center>
+          {/* 上記で定義したモデルローダーで表示される3Dモデル */}
+          <Model mesh={mesh} model_url={props.model_url} position={[0, 0, 0]} />
+        </Center>
         <Preload all />
         {/* 遠近カメラ */}
         <PerspectiveCamera
@@ -46,11 +72,20 @@ function ModelView(props: ModelViewProps) {
           args={[35, window.innerWidth / window.innerHeight, 0.1, 2000]}
           position={[-5, 4, 5]}
         />
+        <ambientLight color="white" intensity={0.5} />
+        <directionalLight color="white" intensity={0.8} position={[1, 5, 5]} />
+        <ContactShadows
+          opacity={0.7}
+          scale={15}
+          blur={2.2}
+          far={20}
+          frames={1}
+          resolution={256}
+          position={[0, -0.4, 0]}
+        />
       </Suspense>
       {/* マウスでの操作を可能にする */}
       <OrbitControls makeDefault ref={controlRef} />
-      {/* xyz軸を表示 */}
-      <axesHelper />
     </>
   );
 }
